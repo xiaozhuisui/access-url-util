068e79e1caba27c9c1c54b22de0ac0e23de941cb
比升-郑圳宏 <zhenhong.zzh@bitsun-inc.com>
2023-08-26 20:46:49 +0800

    去除无用代码

--- bin/index.ts
+++ bin/index.ts
@@ -3,7 +3,7 @@ const { i18nUtil } = require("../lib/index");
 
 const pathname = process.argv[2];
 const srcIndex = pathname.indexOf("src");
-if (!pathname || !pathname.includes("src")) {
+if (!pathname) {
   console.log("请输入路径名参数");
   process.exit(1);
 } else if (!pathname.includes("src")) {
